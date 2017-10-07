1f7b3917213561601055f006d89087a2d39a60d8
Lucas Linard <contato@linard.tech>
2017-10-07 20:57:26 -0300

    Initial commit

--- README.md
+++ README.md
@@ -3,18 +3,13 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+The memory game is a project for Udacity Frontend Nanodegree.
+Users can interact with the game using mouse clicks. Try to get matching pictures with as few as possible moves to get higher scores.
 
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+This repository is based on the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
-/*
- * Create a list that holds all of your cards
- */
+// Start Game
+let gameStarted = false;
 
+//DOM Elements
+let deck = $(".deck");
+let list = $(".deck").children().toArray();
+let stars = $(".stars");
+let restartButton = $(".restart");
+let moves = $(".moves");
+
+// VARIABLES
+let starCount;
+let starScore;
+let items = shuffle(list);
+let count;
+let matches = 0;
+let openedCards;
+let resetPair = false;
+
+function startGame() {
+    gameStarted = false;
+    chronoReset();
+    chronoStop();
+    count = 0;
+    starCount = 0;
+    starScore = 3;
+    items = shuffle(list);
+    matches = 0;
+    openedCards = [0,0];
+    resetPair= true;
+    moves.text("0");
+
+    // Empty deck
+    deck.empty();
+
+    // Iterate through the array and append each item to the list
+    for (let item of items) {
+        $(item).removeClass("open show match");
+        deck.append(item);
+    }
+}
+
+startGame();
+
+restartButton.on('click', function () {
+    startGame();
+});
 
 /*
  * Display the cards on the page
@@ -25,7 +68,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +78,133 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// Set up event delegation to list item
+deck.on('click', 'li', function() {
+
+    if (!gameStarted) {
+        gameStarted = true;
+        chronoStart();
+    }
+
+    let currentCardClass = $(this).attr("class");
+    if (currentCardClass == "card") {
+        flipCard(this);
+        addToOpenedCards(this);
+    }
+
+});
+function flipCard(item) {
+    $(item).toggleClass("open show");
+}
+function matchCard(item) {
+    $(item).toggleClass("match");
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that plays again
+var btn = document.getElementById("play-again");
+
+// When the user clicks on <span> (x), close the modal
+btn.onclick = function() {
+    modal.style.display = "none";
+    startGame();
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function addToOpenedCards(card) {
+    if (resetPair) {
+        flipCard(openedCards[0]);
+        flipCard(openedCards[1]);
+        openedCards = [0,0];
+        resetPair = false;
+    }
+    if (!openedCards[0]) {
+        openedCards[0] = card;
+    } else {
+        count++;
+        starCount++;
+        if (starCount > 12 && starScore > 0 ) {
+            stars.children("li:first").remove();
+            starScore--;
+            starCount = 0;
+        }
+        moves.text(count);
+        openedCards[1] = card;
+        if ($(openedCards[0]).children("i").attr("class") == $(openedCards[1]).children("i").attr("class")) {
+            matchCard(openedCards[0]);
+            matchCard(openedCards[1]);
+            matches++;
+            resetPair = false;
+            if (matches == 1) {
+                chronoStop();
+                document.getElementById("end-time").innerHTML = time
+                document.getElementById("end-moves").innerHTML = count + " moves"
+                document.getElementById("end-stars").innerHTML = starScore + " stars."
+                modal.style.display = "block";
+            }
+        } else {
+            resetPair = true;
+        }
+    }
+}
+
+let time
+// time functions
+var startTime = 0
+var start = 0
+var end = 0
+var diff = 0
+var timerID = 0
+function chrono(){
+    end = new Date()
+    diff = end - start
+    diff = new Date(diff)
+    var msec = diff.getMilliseconds()
+    var sec = diff.getSeconds()
+    var min = diff.getMinutes()
+    var hr = diff.getHours()-1
+    if (min < 10){
+        min = "0" + min
+    }
+    if (sec < 10){
+        sec = "0" + sec
+    }
+    if(msec < 10){
+        msec = "00" +msec
+    }
+    else if(msec < 100){
+        msec = "0" +msec
+    }
+    time = hr + ":" + min + ":" + sec + ":" + msec
+    document.getElementById("chronotime").innerHTML = time
+    timerID = setTimeout("chrono()", 10)
+}
+function chronoStart(){
+    start = new Date()
+    chrono()
+}
+
+function chronoStop(){
+    clearTimeout(timerID)
+}
+function chronoReset(){
+    document.getElementById("chronotime").innerHTML = "0:00:00:000"
+    start = new Date()
+}
